93e3b658e0b02592ab4e2f3bc6188961b14fcfcc
Andres Vasquez <gustavovasquez15@gmail.com>
2023-01-15 11:31:48 -0500

    modificaciones

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,21 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  numeroDocumento: boolean = true;
+  codigo: boolean = false;
+  nuevaContra: boolean = false;
 
-  constructor(private _router: Router) {
-  }
+  recordarContra = {
+    numeroDocumento: '',
+    codigo: ''
+  };
 
-  ngOnInit() {
-  }
+  constructor(private _router: Router) {}
+
+  ngOnInit() {}
 
   ingresar() {
     this._router.navigate(['/inicio']);
   }
 
+  enviarDocumuento() {
+    if (this.recordarContra.numeroDocumento == '') {
+      this.alertas('Error!', 'Ingrese su número de documento', 'error');
+    } else if (this.recordarContra.numeroDocumento) {
+      this.numeroDocumento = false;
+      this.codigo = true;
+      this.recordarContra.numeroDocumento = '';
+    }
+  }
+
+  enviarCodigo() {
+    if(this.recordarContra.codigo==''){
+      this.alertas('Error', 'Ingrese el codigo enviado', 'error');
+    }else if(this.recordarContra.codigo){
+      this.nuevaContra = true;
+      this.codigo = false;
+    }
+  }
+
+  guardarContra() {
+    this.numeroDocumento = true;
+    this.codigo = false;
+    this.nuevaContra = false;
+  }
+
+  alertas(title: string, text: string, icon: any){
+     Swal.fire({
+      title: title,
+      text: text,
+      icon: icon,
+      confirmButtonText: 'Ok!'
+    })
+  }
 }
